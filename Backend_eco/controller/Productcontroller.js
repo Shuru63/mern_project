23942c5116b90c6b67c5ad180933c6064fc0cee9
Shuru63 <shubhamkuargarg63@gmail.com>
2024-04-21 20:57:01 +0530

    create crud api

--- Backend_eco/controller/Productcontroller.js
+++ Backend_eco/controller/Productcontroller.js
@@ -1,8 +1,61 @@
-const product = require("../modal/productmodal");
-
+const Product = require("../modal/productmodal");
+// create the product
+const createProduct = async (req, res) => {
+    const createProducts = await Product.create(req.body)
+    res.status(200).json({ success: true, product: createProducts });
+}
+// get all product in database
 const getAllRouter = async (req, res) => {
-    // const products = await product.find();
-    res.status(200).json({ message: "Route is working", success: true });
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
 };
+// update the product
+const updateproduct = async (req, res) => {
+    let productupdate = Product.findById(req.params.id);
+    if (!productupdate) {
+        return res.status(500).json({
+            success: flase
+            , message: "product is not find"
+        });
+    }
+    productupdate = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({ success: true, productupdate })
+}
+// delete product
+const deleteProduct = async (req, res) => {
+    const productDelete = await Product.findById(req.params.id);
+    if (!productDelete) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+    await Product.deleteOne({ _id: req.params.id });
+    res.status(200).json({
+        success: true,
+        message: "Product is deleted successfully"
+    });
+}
+const getProductDetails= async (req,res) =>{
+ const singleproduct=await Product.findById(req.params.id);
+ if(!singleproduct){
+    return res.status(404).json({
+        success:false,
+        message:"product is not found "
+    })
 
-module.exports = getAllRouter;
+ }  
+   res.status(200).json({
+        success:true,
+        singleproduct
+    })
+}
+module.exports = { getAllRouter,
+     createProduct, 
+     updateproduct,
+      deleteProduct,
+     getProductDetails };
